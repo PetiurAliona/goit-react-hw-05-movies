498e487d1dd3180dbe3e1db10eab37003a387c7c
PetiurAliona <alena.petyur@gmail.com>
2021-09-03 00:34:19 +0300

    deleted comments

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,9 @@
 import api from "../components/utills/apiService"
 import { useState, useEffect } from "react"
-import { useLocation } from "react-router-dom"
 import MoviesList from "../components/MoviesList/MoviesList"
 
 export default function HomePage() {
   const [movies, setMovies] = useState([])
-  const location = useLocation()
 
   useEffect(() => {
     api.fetchMovieTrending().then(({ results }) => setMovies((prev) => [...prev, ...results]))
